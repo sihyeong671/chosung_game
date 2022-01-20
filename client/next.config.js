d88a8b2d31208fad456ffd08b4d480399a4109fd
sihyeong671 <bshlab671@naver.com>
2022-01-20 15:50:33 +0900

    add layout, css-in-js lib

---
+++ client/next.config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+module.exports = {
+  reactStrictMode: true,
+  plugins: ["styled-jsx/babel"]
+}
---
+++ client/pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home(){
+
+
+  
+  return(
+    <div className="main_screen">
+      <div className='photo_wrapper'>
+        <div className='photo'>사진</div>
+      </div>
+      <div className='login_wrapper'>
+        <div>
+          <div>
+            게스트 로그인
+          </div>
+          <div>
+            카카오 로그인
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .login_wrapper{
+          display: flex;
+          flex-grow: 1;
+          align-items: center;
+          justify-content: center;
+        }
+        .main_screen{
+          display: flex;
+        }
+        .photo_wrapper{
+          display: flex;
+          flex-grow: 1;
+
+        }
+        .photo{
+          background-color: aqua;
+          
+        }
+      `}</style>
+    </div>
+  )
+}
