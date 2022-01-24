d9a5e859d577a3c38d3925e03217e7ffd4490090
sihyeong671 <bshlab671@naver.com>
2022-01-24 17:28:14 +0900

    add kakao login

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,35 +6,67 @@ import {BACKGROUND_IMG} from '../public/img/background_img.jpg'
 import { Color } from '../utils/color/colors'
 import VerticalLayout from '../components/VerticalLayout'
 import { connectSocket } from '../utils/socket/socketManger'
+import KaKaoLogin from 'react-kakao-login';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 export default function Home() {
-  
+  const router = useRouter();
+
+  useEffect(() => {
+    if(typeof window !== "undefined"){
+      window.Kakao.init("21ae8256364207043d3cf2492252d388");
+    }
+  }, []);
+
+
   return(
     <div className="main_screen">
       <VerticalLayout>
         <div className='main_title'>
           제목이 여기에 들어오는 건 어떤가요?
-          {/* <div className='photo'></div> */}
         </div>
         <div className='login_wrapper'>
-          <div>
-            <Link href={'/lobby'}>
-              <button className='guest_login' onClick={()=>{
-                sessionStorage.setItem('guest', 'true')
-                connectSocket()
-              }}>
-                게스트 로그인
-              </button>
-            </Link>
-            {/* <div>
-              카카오 로그인
-            </div> */}
-          </div>
+          <Link href={'/lobby'}>
+            <button className='guest_login' onClick={()=>{
+              sessionStorage.setItem('guest', 'true')
+              connectSocket()
+            }}>
+              게스트 로그인
+            </button>
+          </Link>
+          <KaKaoLogin
+            token={String("21ae8256364207043d3cf2492252d388")}
+            onSuccess={(re)=>{
+              console.log('로그인 성공',re);
+              connectSocket()
+              sessionStorage.setItem('guest', 'false')
+              sessionStorage.setItem('nickname',re.profile.properties.nickname);
+              router.push('/lobby');
+            }}
+            onFail={(err)=>{console.log("로그인 실패", err)}}
+            onLogout={()=>{console.log("로그아웃")}}
+            render={({ onClick }) => {
+              return (
+                <button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    onClick();
+                  }}
+                >
+                  카카오로 로그인하기
+                </button>
+              );
+            }}
+            />
+          
         </div>
       </VerticalLayout>
       <style jsx>{`
         .login_wrapper{
           display: flex;
           flex-grow: 1;
+          flex-direction: column; 
           align-items: center;
           justify-content: center;
         }
@@ -71,6 +103,9 @@ export default function Home() {
         .guest_login:hover{
           cursor: pointer;
           color: black;
+        }
+        .kakao:hover{
+          cursor:'pointer';
         }
       `}</style>
     </div>
--- client/pages/test.js
+++ client/pages/test.js
@@ -2,17 +2,31 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { useEffect, useState } from "react";
-
+import KaKaoLogin from 'react-kakao-login';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 export default function App(){
-  const notify = () => toast("Wow so easy!");
+
+  useEffect(() => {
+    if(typeof window !== "undefined"){
+      window.Kakao.init("21ae8256364207043d3cf2492252d388");
+    }
+  }, []);
 
   return (
-    <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
-    </div>
+    <KaKaoLogin 
+      token={String("21ae8256364207043d3cf2492252d388")}
+      onSuccess={()=>{console.log('로그인 성공');}}
+      onFail={(err)=>{console.log("로그인 실패", err)}}
+      onLogout={()=>{console.log("로그아웃")}}
+      render={({onClick})=>(
+        <div onClick={(e)=>{
+          e.preventDefault();
+          onClick();
+        }}>
+          카카오로 로그인하기
+        </div>
+      )}
+    />
   );
 }
